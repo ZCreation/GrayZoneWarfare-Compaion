eac6c854f8863789da823267f60a8beeae989179
ZCreation <i_am_rex910@yahoo.com>
2026-04-05 13:47:19 -0400

    Update Raven Team Mossberg price and ammo type

--- blueprints.js
+++ blueprints.js
@@ -60,8 +60,8 @@ const blueprintLocations = [
     danger: "High",
     missionTip:
       "Interact with the magazine to start the mission, collect the item, then return to Gunny.",
-    weaponPrice: "N/A",
-    ammoType: "N/A",
+    weaponPrice: "6.5k",
+    ammoType: "12-Gauge",
     extractionTip: "Complete mission turn-in with Gunny, then continue weapon unlock at Vendor.",
   },
 ];
